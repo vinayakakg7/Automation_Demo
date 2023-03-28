77f948c8a4f5a894d19166046887a3eff5b2330d
vkg <vkg@gmail.com>
2023-03-28 11:28:44 +0530

    Updated tf  file

--- automation.tf
+++ automation.tf
@@ -7,8 +7,8 @@ variable "aws_secret_key" {
 }
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 data "aws_security_group" "example" {
