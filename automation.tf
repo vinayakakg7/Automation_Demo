93dfc67929f01ccc18214da4eb9aa0a04009ecd7
vkg <vkg@gmail.com>
2023-03-28 12:36:46 +0530

    Updated tf  file

--- automation.tf
+++ automation.tf
@@ -1,9 +1,4 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
   region     = "ap-south-1"
 }
 
